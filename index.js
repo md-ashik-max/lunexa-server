3a6bed7e45aef4595ce01cd95cf13ad23415e05d
md-ashik-max <ar1214697@gmail.com>
2024-08-17 10:24:24 +0600

    added readme.MD file

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+          "http://localhost:5173",
+          "https://lunexa-af8cf.web.app",
+          "https://lunexa-af8cf.firebaseapp.com",
+        ]
+      }
+));
 app.use(express.json());
 
 
@@ -27,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const allProductCollection = client.db("lunexa").collection("allProducts");
         const usersCollection = client.db("lunexa").collection("users");
@@ -108,8 +116,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
